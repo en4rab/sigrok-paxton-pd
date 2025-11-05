21906b6b75dd0aa8b80915d4c77b1497a2ac1862
Robin Bradshaw <en4rab@gmail.com>
2025-11-05 19:54:43 +0000

    Update pd.py
    Simplify decoding and display parity as even/odd rather than error

--- paxton/pd.py
+++ paxton/pd.py
@@ -21,10 +21,6 @@
 import sigrokdecode as srd
 
 
-class SamplerateError(Exception):
-    pass
-
-
 class Decoder(srd.Decoder):
     api_version = 3
     id = 'paxton'
@@ -56,7 +52,7 @@ class Decoder(srd.Decoder):
         ('bits', 'Bits', (0,)),
         ('digits', 'Digits', (1, 2, 5, 6, 7, 8, 9,)),
         ('cards', 'Card', (3,)),
-        ('error', 'Error', (4,)),
+        ('parity', 'Parity', (4,)),
     )
 
     options = (
@@ -67,17 +63,22 @@ class Decoder(srd.Decoder):
     )
 
     def __init__(self):
+        self.STATE_LEADIN = 0
+        self.STATE_LEADOUT = 1
+        self.STATE_DIGITS = 2
+        self.STATE_LRC = 3
+
         self.reset()
 
     def reset(self):
         '''Set global variables to default values called after each packet'''
         self.card_ss = None               # start sequence for card annotations
         self.card_es = None               # end sequence for card annotations
-        self.card_start = None            # flag for start of card data
+        self.card_start = 0               # flag for start of card data
         self.card_decoded = ""            # card numbers to be printed
 
         self.digit_ss = None             # start sequence for digit annotations
-        self.digit_type = 0              # digit type being decoded
+        self.digit_type = self.STATE_LEADIN   # digit type being decoded
 
         self.bit_ss = None               # start sequence for digit annotations
         self.bit = None                  # current bit
@@ -95,13 +96,12 @@ class Decoder(srd.Decoder):
         ones_count = sum(self.bits)
         # Check if the parity is odd
         if ones_count % 2 == 1:
-            return True  # Parity is correct
+            return True  # Parity is odd
         else:
-            return False  # Parity is incorrect
+            return False  # Parity is even
 
     def _calculate_lrc(self):
         '''Calculate LRC over received digits and return it as bit list'''
-        print("Calculating LRC")
         # Initialise a list to store the even parity bits
         even_parity_bits = [0, 0, 0, 0]
         # Calculate even parity bits for each position
@@ -119,13 +119,16 @@ class Decoder(srd.Decoder):
 
     def _get_num(self):
         '''Get the current digit from bits[] and return it as hex'''
+        if len(self.bits) < 5:
+            return None
         # save the BCD number to a list so we can check the LRC
         self.bcd_list.append(self.bits[:4])
         # check the parity and warn if error
-        parity = self._check_parity()
-        if parity is False:
-            ann = [4, ["Parity Error"]]
-            self.put(self.digit_ss, self.samplenum, self.out_ann, ann)
+        if not self._check_parity():
+            ann = [4, ["Even"]]
+        else:
+            ann = [4, ["Odd "]]
+        self.put(self.digit_ss, self.samplenum, self.out_ann, ann)
 
         dec_val = 8 * self.bits[3] + 4 * self.bits[2] + \
                   2 * self.bits[1] + 1 * self.bits[0]
@@ -143,7 +146,10 @@ class Decoder(srd.Decoder):
 
     def _card_ann(self, ann_type):
         '''Write a card annotation of type ann_type to the trace'''
-        digit = self.card_decoded
+        if not self.card_decoded:
+            digit = "No Data"
+        else:
+            digit = self.card_decoded
         ann = [ann_type, [digit]]
         self.put(self.card_ss, self.card_es, self.out_ann, ann)
         # setup for next number
@@ -162,22 +168,17 @@ class Decoder(srd.Decoder):
         if self.card_ss is None:               # if we arent decoding a card No
             self.card_ss = self.bit_ss         # set start of card data
 
-        Leadin = 0
-        Leadout = 1
-        Digits = 2
-        LRC = 3
-
-        if self.digit_type == Leadin:
+        if self.digit_type == self.STATE_LEADIN:
             leadin = self.options['leadin']
             if len(self.bits) == leadin:
                 ann = [1, [str("Lead in")]]
                 self.put(self.digit_ss, self.samplenum, self.out_ann, ann)
                 # setup to catch digits
                 self.digit_ss = None
-                self.digit_type = Digits
+                self.digit_type = self.STATE_DIGITS
                 self.bits.clear()
 
-        if self.digit_type == Leadout:
+        if self.digit_type == self.STATE_LEADOUT:
             # if the packet is done reset for another one
             leadout = self.options['leadout']
             if len(self.bits) == leadout:
@@ -186,7 +187,7 @@ class Decoder(srd.Decoder):
                 self.digit_ss = None
                 self.reset()
 
-        if self.digit_type == Digits:                # handle decoding digits
+        if self.digit_type == self.STATE_DIGITS:       # handle decoding digits
             if len(self.bits) == 5:
                 digit = self._get_num()
 
@@ -201,7 +202,7 @@ class Decoder(srd.Decoder):
                     self._digit_ann(8, digit)          # digit type: end
                     self._card_ann(3)                  # write last card number
                     self._calculate_lrc()              # calc LRC to compare
-                    self.digit_type = LRC              # Next byte is LRC data
+                    self.digit_type = self.STATE_LRC   # Next byte is LRC data
                 elif self.card_start == 1:
                     self._digit_ann(2, digit)          # digit type: digit
                     self.card_decoded += str(digit)    # add digit to card num
@@ -209,7 +210,7 @@ class Decoder(srd.Decoder):
                 else:
                     self._digit_ann(9, digit)          # digit type: unknown
 
-        if self.digit_type == LRC:
+        if self.digit_type == self.STATE_LRC:
             if len(self.bits) == 5:
                 if self.calc_LRC != self.bits:       # LRC matches calculated?
                     ann = [4, ["LRC Error"]]
@@ -217,36 +218,26 @@ class Decoder(srd.Decoder):
                 digit = self._get_num()
                 self._digit_ann(5, digit)
                 # setup to catch last 10 bits
-                self.digit_type = Leadout
+                self.digit_type = self.STATE_LEADOUT
 
     def decode(self):
         '''Capture a bit on the clk capture edge and update on update edge'''
         edge = self.options['edge']
-        if edge == 'rising':
-            capture = 1
-            update = 0
-        else:
-            capture = 0
-            update = 1
+
+        capture_edge = 'r' if edge == 'rising' else 'f'
+        update_edge  = 'f' if edge == 'rising' else 'r'
 
         polarity = self.options['polarity']
-        if polarity == 'normal':
-            low_bit = 0
-            high_bit = 1
-        else:
-            low_bit = 1
-            high_bit = 0
+        bit_map = {0: 1, 1: 0} if polarity == 'inverted' else {0: 0, 1: 1}
 
         while True:
-            # wait for clk edge
-            (clk, dat) = self.wait({0: 'e'})
-            if (clk) == capture:
-                # start new bit
-                self.bit_ss = self.samplenum
-                if (dat) == 0:
-                    self.bit = low_bit
-                elif (dat) == 1:
-                    self.bit = high_bit
-            if (clk) == update:
-                # end bit annotation
-                self._update_state()
+            # wait for clk capture edge
+            (clk, dat) = self.wait({0: capture_edge})
+
+            # start new bit
+            self.bit_ss = self.samplenum
+            self.bit = bit_map[dat]
+
+            self.wait({0: update_edge})
+            # end bit annotation
+            self._update_state()
